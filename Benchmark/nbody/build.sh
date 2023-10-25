f32fb35a591ef48a633a4e69107d5e63ed82278a
Emanuele d'Ajello <55718523+JustJ3di@users.noreply.github.com>
2023-10-25 12:28:40 +0200

    Add files via upload

--- Benchmark/nbody/build.sh
+++ Benchmark/nbody/build.sh
@@ -1,3 +1,5 @@
+rm -f *.o
+rm -f nbodySim
 clang++ -c -o main.o main.cpp 
 clang++ -c -o nbody.o nbody.cpp 
 clang++ -o nbodySim nbody.o main.o
--- Benchmark/nbody/launch.py
+++ Benchmark/nbody/launch.py
@@ -4,16 +4,23 @@ import numpy as np
 
 os.chdir("/Users/emanuele/Desktop/Benchmark/nbody")
 os.listdir()
-n = 10000
+n = 100000
 r = 10 #ogni 10 istanze ho un sample
-subprocess.run(["sh","./launch_nbody.sh","-r",f"{r}","-n",f"{n}"])
-data = []
-with open("data.log","r") as file:
-    for i in file:
-        data.append(int(i))
+for i in range(40):
+    for _ in range(r):
+        subprocess.run(["./nbodySim",f"{n}"])
+    data = []
+    with open("data.log","r") as file:
+        for i in file:
+            data.append(int(i))
         
-data= np.array(data)
-
-print(data)
+    subprocess.run(["rm","data.log"])
+    
+            
+    data= np.array(data)
+    '''
+    QUI INSERIRE I TEST STATISTICI 
+    '''
+    print(data)
 
         
