c05b6f5d7575c78315a6aa3acc25143b9e8e521f
Emanuele d'Ajello <55718523+JustJ3di@users.noreply.github.com>
2023-10-26 13:41:33 +0200

    Add files via upload

--- Benchmark/nbody/launch.py
+++ Benchmark/nbody/launch.py
@@ -4,8 +4,9 @@ import numpy as np
 
 os.chdir("/Users/emanuele/Desktop/Benchmark/nbody")
 os.listdir()
-n = 100000
-r = 10 #ogni 10 istanze ho un sample
+n = 1000
+r =2 #ogni 10 istanze ho un sample
+dataset = []
 for i in range(40):
     for _ in range(r):
         subprocess.run(["./nbodySim",f"{n}"])
@@ -16,11 +17,19 @@ for i in range(40):
         
     subprocess.run(["rm","data.log"])
     
-            
-    data= np.array(data)
-    '''
-    QUI INSERIRE I TEST STATISTICI 
-    '''
+    dataset.append(data)
     print(data)
 
-        
+#print(dataset)
+
+mean_vect = [i for i in range(40)]
+for j in range(40):
+    sample_mean = np.mean(np.array([i for i in dataset[j]]))
+    mean_vect[j] = sample_mean
+
+
+print(mean_vect)
+
+'''
+QUI INSERIRE I TEST STATISTICI 
+'''
